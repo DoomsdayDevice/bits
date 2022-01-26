89e4e6e258dcedafe4e9d29c5907c25ffe69a0a3
doomsday_device <lovesjuicypeaches@gmail.com>
2022-01-26 22:01:14 +0300

    ci/cd

---
+++ utils.ts
@@ -0,0 +1,94 @@
+import { validateSync, ValidationError } from 'class-validator';
+import { plainToClass } from 'class-transformer';
+import {Type} from "@nestjs/common";
+import {Cfg} from "../../config/config.dto";
+
+
+export function formatValErrors(errors: ValidationError[]): string {
+    return errors.map((e, i) => valErrorToStr(e, i === 0)).join('');
+}
+
+function valErrorToStr(
+    err: ValidationError,
+    printClass = true,
+    shouldDecorate = false,
+    hasParent = false,
+    parentPath = ``,
+): string {
+    const boldStart = shouldDecorate ? `\x1b[1m` : ``;
+    const boldEnd = shouldDecorate ? `\x1b[22m` : ``;
+
+    // const boldEnd = shouldDecorate ? `\x1b[22m` : ``;
+    const propConstraintFailed = (propertyName: string): string =>
+        ` - поле ${boldStart}${parentPath}${propertyName}${boldEnd}: ${boldStart}${Object.keys(
+            err.constraints as any,
+        )
+            .map(k => `${err.constraints![k]}`)
+            .join(`,`)}${boldEnd} \n`;
+
+    if (!hasParent) {
+        return (
+            (printClass
+                ? `В объекте класса ${boldStart}${
+                    err.target ? err.target.constructor.name : 'Object'
+                }${boldEnd} не пройдена валидация:\n`
+                : '') +
+            (err.constraints ? propConstraintFailed(err.property) : ``) +
+            (err.children
+                ? err.children
+                    .map(childError => valErrorToStr(childError, false, shouldDecorate, true, err.property))
+                    .join(``)
+                : ``)
+        );
+    }
+    // we format numbers as array indexes for better readability.
+    const formattedProperty = Number.isInteger(+err.property)
+        ? `[${err.property}]`
+        : `${parentPath ? `.` : ``}${err.property}`;
+
+    if (err.constraints) return propConstraintFailed(formattedProperty);
+    return err.children
+        ? err.children
+            .map(childError =>
+                valErrorToStr(
+                    childError,
+                    false,
+                    shouldDecorate,
+                    true,
+                    `${parentPath}${formattedProperty}`,
+                ),
+            )
+            .join(``)
+        : ``;
+}
+
+export function transformAndValidateArraySync<T extends Record<string, any>>(
+    array: any[],
+    Model: Type<T>,
+): T {
+    // replace undefined with null for cases like excelJS
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === undefined) array[i] = null;
+    }
+
+    const transformed = plainToClass(Model, { ...array } as any, {
+        enableImplicitConversion: true,
+    });
+    const valErrors = validateSync(transformed);
+
+    if (valErrors.length > 0) {
+        throw new Error(formatValErrors(valErrors));
+    }
+    return transformed;
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export function validateConfig(config: Cfg): Cfg {
+    const validatedConfig = plainToClass(Cfg, config, { enableImplicitConversion: true });
+    const errors = validateSync(validatedConfig, { skipMissingProperties: false });
+
+    if (errors.length > 0) {
+        throw new Error(formatValErrors(errors));
+    }
+    return validatedConfig;
+}
