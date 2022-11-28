eb7b69b031fb6c1f4921389a48cd524cac8aa2e5
doomsday_device <lovesjuicypeaches@gmail.com>
2022-11-28 16:54:30 +0300

    refactoring

--- db/db.utils.ts
+++ db/db.utils.ts
@@ -1,7 +1,7 @@
 import { Sort } from '../grpc/grpc.dto';
 import { renameKeyNames } from '../bits.utils';
-import { allParsingInstructions, MongoQueryParser } from '@ucast/mongo';
-import { allInterpreters, createSqlInterpreter } from '@ucast/sql';
+// import { allParsingInstructions, MongoQueryParser } from '@ucast/mongo';
+// import { allInterpreters, createSqlInterpreter } from '@ucast/sql';
 import { FindOptionsOrder } from 'typeorm/find-options/FindOptionsOrder';
 
 export function convertGrpcOrderByToTypeorm<T = any>(orderBy: Sort[]): FindOptionsOrder<T> {
@@ -12,14 +12,14 @@ export function convertGrpcOrderByToTypeorm<T = any>(orderBy: Sort[]): FindOptio
   return obj;
 }
 
-export function convertGrpcFilterToUcast(filter: any) {
-  const con = renameKeyNames(filter, { elemMatch: '$elemMatch', eq: '$eq' });
-  const parser = new MongoQueryParser(allParsingInstructions);
-  const parsedCon = parser.parse(con);
-
-  const myInterpret = createSqlInterpreter(allInterpreters);
-
-  // const qb = interpret(parsedCon, getConnection().createQueryBuilder(Activity, 'a'));
-  // const q = qb.getQueryAndParameters();
-  // return qb;
-}
+// export function convertGrpcFilterToUcast(filter: any) {
+//   const con = renameKeyNames(filter, { elemMatch: '$elemMatch', eq: '$eq' });
+//   const parser = new MongoQueryParser(allParsingInstructions);
+//   const parsedCon = parser.parse(con);
+//
+//   const myInterpret = createSqlInterpreter(allInterpreters);
+//
+//   // const qb = interpret(parsedCon, getConnection().createQueryBuilder(Activity, 'a'));
+//   // const q = qb.getQueryAndParameters();
+//   // return qb;
+// }
