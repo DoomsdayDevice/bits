5aeff93181aef4135a475302a78729af531dea4c
doomsday_device <lovesjuicypeaches@gmail.com>
2023-10-26 21:31:52 +0300

    refactor: move eslint

--- packages/eslint-config-common/__tests__/eslint-config-common.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const eslintConfigCommon = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(eslintConfigCommon(), 'Hello from eslintConfigCommon');
-console.info('eslintConfigCommon tests passed');
